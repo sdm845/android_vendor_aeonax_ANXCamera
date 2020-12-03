1001edf07682611f12c61c361ccdf778023dfb02
althafvly <althafvly@gmail.com>
2020-12-03 22:26:44 +0530

    Make it ready for building

--- anx-vendor.mk
+++ anx-vendor.mk
@@ -6,14 +6,10 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/priv-app/ANXScanner/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/ANXScanner/lib) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64)
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/priv-app/MIUICamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MIUICamera/lib)
 
 PRODUCT_PACKAGES += \
-    ANXCamera \
-    ANXExtraPhoto \
-    ANXScanner \
+    MIUICamera \
     anxres
 
 PRODUCT_PROPERTY_OVERRIDES += \
